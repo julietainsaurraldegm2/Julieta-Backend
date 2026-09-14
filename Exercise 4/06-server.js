9f6d991adbd1128015cd18a13c1c1c29ff8ccab6
julietainsaurraldegm2 <julieta.insaurralde@gm2dev.com>
2026-09-14 12:06:30 -0300

    docs(exercise4): translate answers

--- Exercise 4/06-server.js
+++ Exercise 4/06-server.js
@@ -13,18 +13,18 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-//Respuesta de la terminal: Server running at http://127.0.0.1:3000/
+//Terminal response: Server running at http://127.0.0.1:3000/
 
 /* 
-1. Un servidor HTTP es un programa que escucha solicitudes de clientes (como navegadores web) y responde con datos, como páginas web o información,
-   El modulo 'http' de Node.js permite crear servidores HTTP de forma simple
+1. An HTTP server is a program that listens for requests from clients (such as web browsers) and responds with data, such as web pages or information.
+ Node.js's 'http' module makes it simple to create HTTP servers
 
-2. el req(request) es lo que el cliente envía al servidor, como la URL solicitada, los encabezados y los datos del formulario. 
-El res(response) es lo que el servidor envía de vuelta al cliente, como el contenido de la página web, los encabezados y el código de estado
+2. The req (request) is what the client sends to the server, such as the requested URL, headers, and form data. 
+The res (response) is what the server sends back to the client, such as the web page content, headers, and status code
 
-3. Un puerto es un número que identifica de manera única una conexión de red de un dispositivo. Los servidores escuchan en puertos específicos para recibir solicitudes de los clientes
+3. A port is a number that uniquely identifies a network connection on a device. Servers listen on specific ports to receive requests from clients
 
-4. El local host se refiere a el dispositivo en el que se esta ejecutando el servidor
+4. Localhost refers to the device on which the server is running
 
-5. CURL es una herramienta de comandos que sirve para enviar solicitudes al servidor http y recibir respuestas. Es util para probar y depurar servidores web y API's
+5. CURL is a command-line tool used to send requests to an HTTP server and receive responses. It's useful for testing and debugging web servers and APIs
 */
